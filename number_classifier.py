6c4af91654ca6583d8543799e9cd1db713d06c64
VirajA30 <137769122+VirajA30@users.noreply.github.com>
2024-01-11 02:17:15 +0530

    Add files via upload

---
+++ number_classifier.py
@@ -0,0 +1,11 @@
+# Taking input from the user
+num = int(input("Enter a number: "))
+
+# Classifying the number and printing the result
+if num < 0:
+    print("The number is negative.")
+elif num == 0:
+    print("The number is zero.")
+else:
+    print("The number is positive.")
+
